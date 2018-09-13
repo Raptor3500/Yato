69710ceb3b65100386deb109ac0d70a0f48cdc24
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-13 19:01:26 -0400

    Fixed

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ bot = commands.Bot(command_prefix='prefix')
 @bot.event
 async def on_ready():
   print ("------")
-  print ("My name is Yato" + bot.user.name)
-  print ("With the ID:489775956731363328" + bot.user.id)
+  print ("My name is " + bot.user.name)
+  print ("With the ID: " + bot.user.id)
   print ("Using discord.py v" + discord.__version__)
   print ("------")
 
