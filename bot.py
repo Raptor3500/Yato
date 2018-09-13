86f48f77ba0622e1b23d8f070e0d67e05034aae2
AgentHi5 <39109797+AgentHi5@users.noreply.github.com>
2018-09-13 16:05:40 -0400

    bot.py Setup

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
 
+bot = commands.Bot(command_prefix='prefix')
+
+# To remove the help command and make your own help command
+#bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+  print ("------")
+  print ("My name is " + bot.user.name)
+  print ("With the ID: " + bot.user.id)
+  print ("Using discord.py v" + discord.__version__)
+  print ("------")
+  
+ # This is how you will make a command on the Main file
+@bot.commands()
+async def test():
+  await bot.say('test')
+
+
+bot.run('token')
