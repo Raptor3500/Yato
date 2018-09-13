6a90ab92b7587442fbe0dfac617a93b18b61523b
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-13 19:55:03 -0400

    Test if fails with put back

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext.commands import Bot
 import asyncio
 import os
 
-bot = commands.Bot(command_prefix='prefix')
+bot = commands.Bot(command_prefix='yato')
 
 # To remove the help command and make your own help command
 #bot.remove_command('help')
@@ -16,6 +16,20 @@ async def on_ready():
   print ("With the ID: " + bot.user.id)
   print ("Using discord.py v" + discord.__version__)
   print ("------")
+
+@client.command(pass_context=True)
+async def help(ctx)
+author = ctx.message.author
+
+
+embed = discord.Embed(
+  colour = discord.colour.orange()
+)
+
+embed.set_author(name'Help')
+embed.add_field(name= '.ping', value-'Returns Pong!', inline-False)
+
+await client.send_message(author, embed-embed)
 
 
 
