e470b4f548b5f4f6c4f02845356b99e33d325773
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-14 13:08:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def kill(ctx, user: discord.Member=None):
             await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
         elif user.id == ctx.message.author.mention:
           await bot.say(ctx.message.author.mention + ": I have killed {}".format(user.name))
-        elif user.id == "400418638240415745":
+        if user.id == "400418638240415745":
           await bot.say(ctx.message.author.mention + ": God vs a God huh Sekki!! Sorry Im not losing this one")
             
 #invite me
