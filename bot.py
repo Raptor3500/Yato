74ee29a85d0428d3d1363e23f1fc7c5454048175
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-13 20:15:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,26 @@ async def on_ready():
   print ("With the ID: " + bot.user.id)
   print ("Using discord.py v" + discord.__version__)
   print ("------")
+  
+  # Set my game
+@bot.command(hidden=True, pass_context=True)
+async def setgame(ctx, *args):
+    await bot.delete_message(ctx.message)
+    if ctx.message.author.id == unrealismID:
+        game = ' '.join(args)
+        server = ctx.message.server
+        current_status = server.me.status if server is not None else None
+        if game:
+            game = game.strip()
+            await bot.change_presence(game=discord.Game(name=game), status=current_status)
+            await bot.say("Game set to `{}`.".format(game))
+            print ("Game set to '{}'".format(game))
+        else:
+            await bot.change_presence(game=None, status=current_status)
+            await bot.say("Not playing a game now.")
+            print ("Not playing a game")
+    else:
+        await bot.say("You're not allowed to run this command!")
 
 
 
