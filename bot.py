5eb2065159ca3a1073cc757c31a22b5f5e14f8fa
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-15 21:22:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def say(ctx, *args):
     """Make me say your message"""
+    
     if ctx.message.author.id in ownerID:
         channel = ctx.message.channel
         mesg = ' '.join(args)
