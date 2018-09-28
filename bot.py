a29cdb6e07fa106edabf8f6f0576a92ab759fea4
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-09-27 22:17:17 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ async def bfmode(ctx, *args):
   if ctx.message.author.id in ownerID:
     mesg = ' '.join(args)
     await bot.change_presence(game=discord.Game(name= 'with Testings ;)))'))
-    await bot.change_nickname('Testings BF')
+    await bot.change_own_nickname('Testings BF')
          
   
 
