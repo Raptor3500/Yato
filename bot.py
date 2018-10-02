93679d1945dd53e936b8c610271d8f671a7fd322
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-02 11:05:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def bfmode(ctx, *args):
     await bot.change_presence(game=discord.Game(name= 'I am Testings boyfriend'))
     await asyncio.sleep(3)
     await bot.change_presence(game=discord.Game(name= 'With Testings ;)))'))
-    await bot.loop
+    bot.loop
          
   
 
