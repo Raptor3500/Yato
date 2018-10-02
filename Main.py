c157da44cb792b1a666ee4234f990ece60605460
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-02 16:15:11 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,9 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import loadconfig
+import os
 
-startup_extensions = loadconfig.__cogs__
-bot = commands.Bot (command_prefix=loadconfig.__prefix__)
+bot = commands.Bot  (command_prefix='yato '
 
 bot.remove_command('help')
 
@@ -17,6 +16,4 @@ if __name__ == "__main__":
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
 
-# Now that this bot has a repository on GitHub, I have removed the config of this bot.
-# The name of the file has to be "loadconfig.py" or else this bot won't launch.
-bot.run(loadconfig.__token__)
+bot.run(os.environ.get('Token'))
