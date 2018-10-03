5f4b927478b5234bfb225ea68aced97682f0b402
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-02 20:04:28 -0400

    Update message.py

--- cogs/message.py
+++ cogs/message.py
@@ -22,10 +22,8 @@ class message():
             if r['status'] == 'success':
                 await self.bot.send_message(message.channel, r['response'] )
                 
-        if 'I am an adult' in message.content:
-            await self.bot.send_message(message.channel, "pls help")
-            elif message.author.bot.id is '329337654850093056'
-            await self.bot.send_message(message.channel, "No no you are not")
+        if message.author.id is '329337654850093056' and 'I am an adult' in message.content:
+            await self.bot.send_message(message.channel, "No, no you are not")
 
 
 
